dbb173bd941f29c0d80dfa175958ce1d4ed86707
jack-h-3013 <jack_h_93@hotmail.com>
2026-06-23 20:08:22 +0100

    seeing the functions starting to work
    now that that algorithm is starting to come together i can now see that the letters for the 4 square challnge are starting to be visible prooving that i am going in the right direction.

--- wordFinder.js
+++ wordFinder.js
@@ -1,8 +1,6 @@
 import { dictionary } from "./letterCounter.js";
+import { letterCounter } from "./letterCounter.js";
 
-console.log(dictionary.length);
-console.log(dictionary[0]);
-console.log(dictionary[100]);
 // creating the trie style tree
 
 class TrieNode {
@@ -35,4 +33,36 @@ for (let word of dictionary) {
   trie.insert(word);
 }
 
-console.log(letters);
+// filtering the list of words to match the length required
+function getWordsOfLength(words, length) {
+  return words.filter((word) => word.length === length);
+}
+
+// checking if a word can be built using the available letters
+function canIBuildAWord(word, availableLetters) {
+    const letterCounts = { ...availableLetters };
+    
+    for (const letter of word) {
+        if (!letterCounts[letter]) {
+            return false;
+        }
+        letterCounts[letter]--;
+    }
+    return true;
+}
+
+const availableLetters = letterCounter(letters);
+
+const fourLetterWords = getWordsOfLength(dictionary, 4).filter((word) => canIBuildAWord(word, availableLetters));
+
+console.log(fourLetterWords);
+
+
+
+
+
+
+
+
+export { trie };
+
